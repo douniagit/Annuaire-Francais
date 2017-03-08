f1e496994d72a5817bcc965e67aa86acc080360c
dounia <d.fergane@gmail.com>
2017-03-08 15:28:15 +0100

    pull

---
+++ Annuaire/src/grid/Grid.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Grid.css';
+
+
+class Grid extends Component {
+  render() {
+    return (
+      <div className="grid">
+
+      </div>
+    );
+  }
+}
+
+export default Grid;
