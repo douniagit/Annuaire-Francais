0d72eb62083ffde9e2579a70e260a09356d0eb0b
nitramnoe <eon.martin@icloud.com>
2017-03-08 15:25:37 +0100

    ajout de modal box

--- Annuaire/src/App.js
+++ Annuaire/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import Modal from './modal/modal.js';
+import './App.css';
+import wireframe from './img/maquette.jpg'
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Modal></Modal>
       </div>
     );
   }
