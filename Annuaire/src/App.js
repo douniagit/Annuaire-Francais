41fa2d1d1888e86ac082e71111712f9fee8f4b4a
Joackim Balouka <jobalouka@gmail.com>
2017-03-08 15:13:46 +0100

    add App

--- Annuaire/src/App.js
+++ Annuaire/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import Grid from './grid/Grid.jsx';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Grid />
       </div>
     );
   }
 }
+
 
 export default App;
