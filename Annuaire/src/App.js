a275612f488088c7f45bcbf791e755dc1e73b0e2
nitramnoe <eon.martin@icloud.com>
2017-03-08 17:44:16 +0100

    Update App.js

--- Annuaire/src/App.js
+++ Annuaire/src/App.js
@@ -3,9 +3,6 @@ import Card from "./Card/Card.jsx";
 import wireframe from './img/maquette.jpg'
 import Footer from './Footer/Footer.jsx';
 import Header from './Header/Header.jsx';
-
-import Grid from './grid/Grid.jsx';
-
 import Dropdown from './Dropdown/Dropdown.jsx';
 import Input from './Input/Input.jsx';
 import AddRessource from './modal/buttonModal.jsx'
@@ -16,9 +13,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header></Header>
-
-        <Grid />
-
         <Footer></Footer>
         <Dropdown/>
         <Input/>
