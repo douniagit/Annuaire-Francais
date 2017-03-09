2db730a90f1b53cf2393192247c36150dfb476fb
Sadia <sadia93380@gmail.com>
2017-03-09 10:04:21 +0100

    sadia debug

--- Annuaire/src/Header/Header.jsx
+++ Annuaire/src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Header.css';
-import Logo from '/home/simplonco/Documents/Projet-Annuaire/Annuaire-Fran-ais/Annuaire/public/assets/logo-cocoricourse.png';
+import Logo from '../../public/assets/logo-cocoricourse.png';
 
 class Header extends Component {
   render() {
--- Annuaire/src/data/dataRessource.js
+++ Annuaire/src/data/dataRessource.js
@@ -3,101 +3,129 @@ var ressources = {
               'id':"1",
               'name':'Duolingo',
               'description':'site et application pour apprendre le français. Disponible sur IOS et android',
-              'lien':'https://www.duolingo.com'
-},
-{
-              'id':"1",
-              'name':'Babbel',
-              'description':'site et application pour apprendre le français. Disponible sur IOS et android',
-              'lien':'https://fr.babbel.com/'
+              'lien':'https://www.duolingo.com',
+              'images':'https://www.duolingo.com/images/illustrations/owl-happy@2x.png',
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
               'id':"2",
-              'name':'Bonjour de France'
-              'description':'site pour apprendre le français'
-              'lien':'http://www.bonjourdefrance.com/'
+              'name':'Babbel',
+              'description':'site et application pour apprendre le français. Disponible sur IOS et android',
+              'lien':'https://fr.babbel.com/',
+              'images':'https://pic.fyvor.com/20160827/20160827075406_94131.jpg'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
               'id':"3",
+              'name':'Bonjour de France'
+              'description':'site pour apprendre le français',
+              'lien':'http://www.bonjourdefrance.com/',
+              'images':'http://www.synonymes-fle.com/img/logo.jpg'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+},
+{
+              'id':"4",
               'name':'Le point du FLE',
               'description':'site pour apprendre le français',
-              'lien':'http://www.lepointdufle.net/p/apprendre_le_francais.htm'
+              'lien':'http://www.lepointdufle.net/p/apprendre_le_francais.htm',
+              'images':'https://pbs.twimg.com/profile_images/1881545330/lpdf_fbk-nveau_400x400.gif'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"5",
               'name':'Dailyfrenchpod',
               'description':'site pr apprendre le fr',
-              'lien':'http://www.dailyfrenchpod.com/'
+              'lien':'http://www.dailyfrenchpod.com/',
+              'images':'http://www.learningfrenchsecrets.com/images/dailyfrenchpod.png'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"6",
               'name':'Alison',
               'description':'site pr apprendre le fr',
               'lien':'https://alison.com/courses/Basic-French-Language-For-Everyday-Life/content/scorm/4552/module-1-introduction-to-learning-french'
+              'images':'http://3.bp.blogspot.com/-hTBJSQCY7nw/Uu7Ga9weyOI/AAAAAAAAAaI/iK-7jzXYVz4/s1600/alison+logo.png'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"6",
               'name':'BBC',
               'description':'site anglophone pour apprendre le français',
-              'lien':'http://www.bbc.co.uk/languages/french/'
+              'lien':'http://www.bbc.co.uk/languages/french/',
+              'images':'https://s-media-cache-ak0.pinimg.com/564x/99/26/c3/9926c30b2e0cbcd316f4ee4841abe28e.jpg'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"7",
               'name':'Apprendre avec TV5 Monde',
               'description':'site pour apprendre le français',
-              'lien':'http://apprendre.tv5monde.com/'
+              'lien':'http://apprendre.tv5monde.com/',
+              'images':'http://apprendre.tv5monde.com/sites/apprendre.tv5monde.com/files/logo_0.png'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"8",
               'name':'tolearnfrench',
               'description':'site pour apprendre le français',
-              'lien':'http://www.tolearnfrench.com/'
+              'lien':'http://www.tolearnfrench.com/',
+              'images':'http://www.tolearnfrench.com/newlogotest2.gif'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+                'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"9",
               'name':'busuu',
               'description':'site et application ppur apprendre le français',
-              'lien':'https://www.busuu.com/fr/a/a/apprendre-francais-en-ligne'
+              'lien':'https://www.busuu.com/fr/a/a/apprendre-francais-en-ligne',
+              'images':'https://lh3.googleusercontent.com/HYatgtvcl4yS1hrivxt5bpWVgU6gJsbNOYK-B3fTfakaQxhhcDgZUs2-sLHZDlUQedU=h556'
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
 },
 {
+              'id':"10",
               'name':'polarfle',
               'description':' site amusant pour apprendre le français  adultes : Un meurtre a été commis à Trouville. L’inspecteur Roger Duflair mène l’enquête. Il doit retrouver le coupable, son mobile et les circonstances du crime. Vous êtes son assistant. ',
-              'lien':'http://www.polarfle.com/'
+              'lien':'http://www.polarfle.com/',
+              'images':'http://1.bp.blogspot.com/-PDfMKEV_xA0/Uz5VCa9xh3I/AAAAAAAAAPE/O48W9_2KCcU/s1600/PolarFle.gif'
+              'langues':['french','spanish','english'],
+              'niveau':[],
+              'ages':[],
+              'format':[],
+              'keywords':['langue','étranger','français','vidéo','niveau','facile']
             },]
 
-images: [{
-'nameImg':'Duolingo',
-  'https://www.duolingo.com/images/illustrations/owl-happy@2x.png'},
-{
-'nameImg':'Babbel',
-'https://pic.fyvor.com/20160827/20160827075406_94131.jpg'
-},
-{
-'nameImg':'Bonjour de France',
-'http://www.synonymes-fle.com/img/logo.jpg'
-},
-{
-'nameImg':'Le point du FLE',
-'https://pbs.twimg.com/profile_images/1881545330/lpdf_fbk-nveau_400x400.gif'
-},
-{
-'nameImg':'Dailyfrenchpod',
-'http://www.learningfrenchsecrets.com/images/dailyfrenchpod.png'
-'http://3.bp.blogspot.com/-hTBJSQCY7nw/Uu7Ga9weyOI/AAAAAAAAAaI/iK-7jzXYVz4/s1600/alison+logo.png'
-},
-{
-'nameImg':'Alison',
-'https://s-media-cache-ak0.pinimg.com/564x/99/26/c3/9926c30b2e0cbcd316f4ee4841abe28e.jpg'
-},
-{
-'nameImg':'Tv5monde'
-'http://apprendre.tv5monde.com/sites/apprendre.tv5monde.com/files/logo_0.png'
-},
-{
-'nameImg':'tolearnfrench',
-'http://www.tolearnfrench.com/newlogotest2.gif'
-},
-{
-'nameImg':'busuu',
-'https://lh3.googleusercontent.com/HYatgtvcl4yS1hrivxt5bpWVgU6gJsbNOYK-B3fTfakaQxhhcDgZUs2-sLHZDlUQedU=h556'
-},
-{
-'nameImg':'polarfle',
-'http://1.bp.blogspot.com/-PDfMKEV_xA0/Uz5VCa9xh3I/AAAAAAAAAPE/O48W9_2KCcU/s1600/PolarFle.gif'
-},
-]
             }
