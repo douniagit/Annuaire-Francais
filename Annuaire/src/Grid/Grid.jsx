f1906baecc101b05d67f6993b4978d139b8fee24
nitramnoe <eon.martin@icloud.com>
2017-03-09 11:11:33 +0100

    'add addRessource to grid'

--- Annuaire/src/Grid/Grid.jsx
+++ Annuaire/src/Grid/Grid.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './Grid.css';
 import Card from "../Card/Card.jsx";
-import './Grid.css';
 import Dropdown from '../Dropdown/Dropdown.jsx';
 import Input from '../Input/Input.jsx';
+import AddRessource from '../modal/buttonModal.jsx';
 
 class Grid extends Component {
   render() {
@@ -12,6 +12,7 @@ class Grid extends Component {
         <Dropdown />
         <Input />
         <Card />
+        <AddRessource />
       </div>
     );
   }
