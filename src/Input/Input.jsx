324f396b82e7b3c7e540f86da97d64c63167ed4f
Sadia <sadia93380@gmail.com>
2017-03-13 17:51:50 +0100

    mise a jour

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -5,13 +5,13 @@ class Input extends Component {
   render() {
     return (
       <div className="input">
-<<<<<<< HEAD
+
         <input type="text" className="search-bar" placeholder="Type your query here" />
-=======
+
         <span className="border-anim">
           <input type="text" className="search-bar" placeholder="Type your query here" />
         </span>
->>>>>>> 1671982963b2c09febecfc1fcbe783087ee414a3
+
       </div>
     );
   }
