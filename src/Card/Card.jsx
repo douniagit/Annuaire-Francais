229f8381e44e7664b3f817d987c6688b3dc322e6
nitramnoe <eon.martin@icloud.com>
2017-03-14 14:42:21 +0100

    martin

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -11,26 +11,25 @@ class Card extends React.Component {
           <h4>150 ressources gratuites à disposition </h4>
           {data.site.map(info =>
           <div className="contentCard">
-                <div className="Card">
-                  <img src={info.images} alt="cocoricoPicture" className="img"/>
-                  <div className="text">
-                    <div className="title">{info.name}</div>
-                    <div className="desc">{info.description}</div>
-                  </div>
-                  <div className="wrapp">
-                    <div className="icones">
-                      <img src="#" alt="langue"/>
-                      <img src="#" alt="niveau"/>
-                      <img src="#" alt="format"/>
-                    </div>
-                  </div>
+            <div className="Card">
+              <img src={info.images} alt="cocoricoPicture" className="img"/>
+              <div className="text">
+                <div className="title">{info.name}</div>
+                <div className="desc">{info.description}</div>
+              </div>
+              <div className="wrapp">
+                <div className="icones">
+                  <img src="#" alt="langue"/>
+                  <img src="#" alt="niveau"/>
+                  <img src="#" alt="format"/>
                 </div>
+              </div>
+            </div>
+
           </div>
         )
         }
       </div>
-
-
     );
   }
 }
