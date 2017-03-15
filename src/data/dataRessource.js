dd41b9a84a876152075ba91e4ea6683e4b0d912f
Sadia <sadia93380@gmail.com>
2017-03-15 12:49:53 +0100

    mise a jour des keywords for Martin

--- src/data/dataRessource.js
+++ src/data/dataRessource.js
@@ -7,10 +7,11 @@ var ressources = {
               'images':'https://www.duolingo.com/images/illustrations/owl-happy@2x.png',
               'langues':['French','Spanish','English','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Croatian','Korean','Vietnamese','Indonesian','Turkish',
             'Finnish','Romanian','Greek','Danish','Japanese','Russian','Italian','German','Punjabi','Polish','Tagalog','Bengali','Thai','Tamil','Telugu'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile'],
+              'keywords':['French','Spanish','English','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Croatian','Korean','Vietnamese','Indonesian','Turkish',
+              'Finnish','Romanian','Greek','Danish','Japanese','Russian','Italian','German','Punjabi','Polish','Tagalog','Bengali','Thai','Tamil','Telugu','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices'],
 },
 {
               'id':"2",
@@ -19,10 +20,10 @@ var ressources = {
               'lien':'https://fr.babbel.com/',
               'images':'https://pic.fyvor.com/20160827/20160827075406_94131.jpg',
               'langues':['French','English','Deutsch','Italian','Portuguese','Svenska'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Youngs','Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Babbel','French','English','Deutsch','Italian','Portuguese','Svenska','Beginner','Intermediate','Confirmed','Youngs','Teenagers','Adults','Exercices']
 },
 {
               'id':"3",
@@ -31,10 +32,10 @@ var ressources = {
               'lien':'http://www.bonjourdefrance.com/',
               'images':'http://www.synonymes-fle.com/img/logo.jpg',
               'langues':['French'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices','Games','Lesson'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Bonjour de France','French','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices','Games','Lesson']
 },
 {
               'id':"4",
@@ -43,10 +44,10 @@ var ressources = {
               'lien':'http://www.lepointdufle.net/p/apprendre_le_francais.htm',
               'images':'https://pbs.twimg.com/profile_images/839829594664480768/bf2uSbge.jpg',
               'langues':['French'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices','Lesson','Audios'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Le point du FLE','Teenagers','Adults','Exercices','Lesson','Audios','Beginner','Intermediate','Confirmed','French']
 },
 {
               'id':"5",
@@ -55,10 +56,10 @@ var ressources = {
               'lien':'http://www.dailyfrenchpod.com/',
               'images':'http://www.learningfrenchsecrets.com/images/dailyfrenchpod.png',
               'langues':['English'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Youngs','Teenagers','Adults'],
               'format':['Exercices','Audios','Lessons','Videos'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Dailyfrenchpod','Youngs','Teenagers','Adults','Exercices','Audios','Lessons','Videos','English','Beginner','Intermediate','Confirmed']
 },
 {
               'id':"6",
@@ -67,10 +68,10 @@ var ressources = {
               'lien':'https://alison.com/courses/Basic-French-Language-For-Everyday-Life/content/scorm/4552/module-1-introduction-to-learning-french',
               'images':'http://3.bp.blogspot.com/-hTBJSQCY7nw/Uu7Ga9weyOI/AAAAAAAAAaI/iK-7jzXYVz4/s1600/alison+logo.png',
               'langues':['French','English'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices','Lesson','Articles'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Alison','French','English','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices','Lesson','Articles']
 },
 {
               'id':"6",
@@ -79,10 +80,10 @@ var ressources = {
               'lien':'http://www.bbc.co.uk/languages/french/',
               'images':'https://s-media-cache-ak0.pinimg.com/564x/99/26/c3/9926c30b2e0cbcd316f4ee4841abe28e.jpg',
               'langues':['French','English'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Youngs','Teenagers','Adults'],
               'format':['Exercices','Games','Audios','Articles','Quizz'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['BBC','French','English','Beginner','Intermediate','Confirmed','Youngs','Teenagers','Adults','Exercices','Games','Audios','Articles','Quizz']
 },
 {
               'id':"7",
@@ -91,10 +92,10 @@ var ressources = {
               'lien':'http://apprendre.tv5monde.com/',
               'images':'http://apprendre.tv5monde.com/sites/apprendre.tv5monde.com/files/logo_0.png',
               'langues':['French','Spanish','English','Arabic','Deutsch','Japanese','Korean','Vietnamese'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Youngs','Teenagers','Adults'],
               'format':['Exercices','Audios','Quizz','Lesson',],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Apprendre avec TV5 Monde','French','Spanish','English','Arabic','Deutsch','Japanese','Korean','Vietnamese','Beginner','Intermediate','Confirmed','Youngs','Teenagers','Adults','Exercices','Audios','Quizz','Lesson']
 },
 {
               'id':"8",
@@ -103,10 +104,10 @@ var ressources = {
               'lien':'http://www.tolearnfrench.com/',
               'images':'http://www.tolearnfrench.com/newlogotest2.gif',
               'langues':['French','Spanish','English','Deutsch','Italian','Dutch'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices','Lesson','Quizz'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['ToLearnFrench','French','Spanish','English','Deutsch','Italian','Dutch','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices','Lesson','Quizz']
 },
 {
               'id':"9",
@@ -115,10 +116,10 @@ var ressources = {
               'lien':'https://www.busuu.com/fr/a/a/apprendre-francais-en-ligne',
               'images':'https://lh3.googleusercontent.com/HYatgtvcl4yS1hrivxt5bpWVgU6gJsbNOYK-B3fTfakaQxhhcDgZUs2-sLHZDlUQedU=h556',
               'langues':['French','Spanish','English','Deutsch','Italian','Portuguese','Turkish','Arabic','Polish','Russian','Japanese','Chinese'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Busuu','French','Spanish','English','Deutsch','Italian','Portuguese','Turkish','Arabic','Polish','Russian','Japanese','Chinese','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices']
 },
 {
               'id':"10",
@@ -127,10 +128,10 @@ var ressources = {
               'lien':'http://www.polarfle.com/',
               'images':'http://1.bp.blogspot.com/-PDfMKEV_xA0/Uz5VCa9xh3I/AAAAAAAAAPE/O48W9_2KCcU/s1600/PolarFle.gif',
               'langues':['French','Spanish','English]','Deutsch'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices','Games','Audios'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Polarfle','French','Spanish','English]','Deutsch','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices','Games','Audios']
             },
             {
               'id':"11",
@@ -140,10 +141,11 @@ var ressources = {
               'images':'https://www.50languages.com/images-portal/learn-languages-free-online-app.png',
               'langues':['French','English','Spanish','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Croatian','Korean','Vietnamese','Indonesian','Turkish',
             'Finnish','Romanian','Greek','Danish','Japanese','Russian','Italian','German','Bosnian','Danish','Urdu','Kannada','Macedonian','Marathi','Lithuanian','Latviam','Catalan','Esperanto','Estonian','Slovenian','Slovak','Serbian','Albanian','Figrinya','Armenian'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['50Languages','French','English','Spanish','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Korean','Vietnamese','Indonesian','Turkish',
+            'Finnish','Danish','Japanese','Russian','Italian','German','Bosnian','Danish','Urdu','Kannada','Lithuanian','Catalan','Esperanto','Slovenian','Slovak','Serbian','Albanian','Figrinya','Armenian','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices']
             },
             {
               'id':"12",
@@ -152,10 +154,10 @@ var ressources = {
               'lien':'https://francais-langue-daccueil.org/',
               'images':'https://www.helloasso.com/assets/img/logos/francais-langue-d-accueil.jpg',
               'langues':['French'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Lesson','Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['FLA','French','Beginner','Intermediate','Confirmed','Teenagers','Adults','Lesson','Exercices']
             },
             {
               'id':"13",
@@ -164,10 +166,10 @@ var ressources = {
               'lien':'http://www.podcastfrancaisfacile.com/',
               'images':'https://yt3.ggpht.com/-z8801R7iFFE/AAAAAAAAAAI/AAAAAAAAAAA/uaefGt2Y7Yg/s100-c-k-no-mo-rj-c0xffffff/photo.jpg',
               'langues':['French'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercies','Videos','Audios'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Podcast Français Facile','French','Beginner','Intermediate','Confirmed','Exercies','Videos','Audios']
             },
             {
               'id':"14",
@@ -176,10 +178,10 @@ var ressources = {
               'lien':'https://www.youtube.com/channel/UCVgW9ZQaGBk6fsiPgE2mYDg',
               'images':'http://www.waxtailor.com/wp-content/uploads/2016/06/YouTube-logo-full_color.png',
               'langues':['French'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Videos'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Learn French with Pierre in videos','French','Beginner','Intermediate','Confirmed','Teenagers','Adults','Videos','Blog']
             },
             {
               'id':"15",
@@ -188,10 +190,10 @@ var ressources = {
               'lien':'https://www.frantastique.com/fr',
               'images':'http://www.eyepreferparis.com/.a/6a00d83451b0bd69e201bb092277b8970d-pi',
               'langues':['French','English','Deutsch','Italian','Portuguese','Svenska','Nederlands','Hebrew','Russian','Arabic','Chinese','Japanese'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Frantastique','French','English','Deutsch','Italian','Portuguese','Svenska','Nederlands','Hebrew','Russian','Arabic','Chinese','Japanese','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices']
             },
             {
               'id':"16",
@@ -201,10 +203,11 @@ var ressources = {
               'images':'https://lh3.googleusercontent.com/14037RbzbMHOZlmMczxG5s-_CN6KmUqh7fObaJ0tRiK6zzCkIiyV_dcyMUNMBPQW-g=w300',
               'langues':['French','English','Spanish','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Croatian','Korean','Vietnamese','Indonesian','Turkish',
             'Finnish','Romanian','Greek','Danish','Japanese','Russian','Italian','German'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Mondly','French','English','Spanish','Czech','Portuguese','Hindi','Polish','Ukrainian','Arabic','Persian','Bulgarian','Swedish','Afrikaans','Norwegian','Hungarian','Dutch','Hebrew','Chinese','Croatian','Korean','Vietnamese','Indonesian','Turkish',
+            'Finnish','Romanian','Greek','Danish','Japanese','Russian','Italian','German','Beginner','Intermediate','Confirmed','Teenagers','Adults','Exercices']
             },
             {
               'id':"17",
@@ -213,10 +216,10 @@ var ressources = {
               'lien':'https://play.google.com/store/apps/details?id=free.langame_fr.rivex',
               'images':'http://1.bp.blogspot.com/-zbVC-FWG9J0/USLwqhloW6I/AAAAAAAADxs/HQVsgQ6x5Y0/s1600/Screen+Shot+2013-02-18+at+10.25.10+PM.png',
               'langues':['French','English'],
-              'niveau':['Beginner','Medium','Confirmed'],
+              'niveau':['Beginner','Intermediate','Confirmed'],
               'ages':['Youngs','Teenagers','Adults'],
               'format':['Exercices'],
-              'keywords':['langue','étranger','français','vidéo','niveau','facile']
+              'keywords':['Learn & play','French','English','Beginner','IIntermediate','Confirmed','Youngs','Teenagers','Adults','Exercices']
             },
 
 ]
