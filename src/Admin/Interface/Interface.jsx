9e68817848a28eeb13806585579a149ce141546e
Nasserdine <nasserdine.bouguerne@gmail.com>
2017-03-16 13:35:53 +0100

    modif admin

---
+++ src/Admin/Interface/Interface.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from '../Components/InterfaceComponents/Header/Header.jsx'
+import Selection from '../Components/InterfaceComponents/Selection/Selection.jsx'
+
+
+class Interf extends Component {
+  render() {
+    return (
+      <div className="selection">
+        <Header />
+        <Selection />
+
+
+      </div>
+    );
+  }
+}
+
+
+export default Interf;
