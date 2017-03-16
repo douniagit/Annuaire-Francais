660300b82c14472d00e2ee5bcdc9c51a788237a9
Joackim Balouka <jobalouka@gmail.com>
2017-03-16 12:43:44 +0100

    commission

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ const routes=require("./routes.js");
 
 app.use( function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");// www.google.com monsite.com 143.45.78.23
+  response.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
 app.use(routes);
 
 app.listen(3000,(err)=>{
-	console.log('CA MARCHE!!!!');
+	console.log('CA MARCHE!!!! :3');
 })
-
