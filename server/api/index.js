30ffe420bf5718fab2e5015c506302891257045d
dounia <d.fergane@gmail.com>
2017-03-16 18:10:54 +0100

    debug

--- server/api/index.js
+++ server/api/index.js
@@ -4,17 +4,11 @@ const users = require('./controllers/users');
 const ressources= require('./controllers/ressources');
 const suggestions= require('./controllers/suggestions');
 const bodyParser=require('body-parser');
-<<<<<<< HEAD
-const {Router} =require('express');
-const db=require('../database');
 
-const apiRoutes=new Router();
-=======
 const { Router } = require('express');
 const db = require('../database/index.js');
 
 const apiRoutes = new Router();
->>>>>>> c30592cea94d1083d60d84826a0b4bd17aff35d5
 // pour définir les routes
 
 apiRoutes.use(bodyParser.urlencoded({extended:false}));
@@ -33,21 +27,11 @@ apiRoutes.get('/users', function (req,res){
 });
 
 apiRoutes.put('/users', function (req,res){
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> c30592cea94d1083d60d84826a0b4bd17aff35d5
 	db.users.update(req,res);
 });
 
 apiRoutes.delete('/users', function (req,res){
-<<<<<<< HEAD
-	
-	db.users.delete(req,res);
-});
-=======
->>>>>>> c30592cea94d1083d60d84826a0b4bd17aff35d5
 
 	db.users.delete(req,res);
 });
@@ -61,13 +45,9 @@ apiRoutes.post('/ressources', function (req,res){ //ou .get?
 
 apiRoutes.get('/ressources', function (req,res){
 	//methode de controller
-<<<<<<< HEAD
-	db.ressources.find(req,res);
-=======
 	console.log('Bonjour je suis la route get ressources');
 	db.ressources.find(req,res);
 	console.log('Je suis la méthode find.');
->>>>>>> c30592cea94d1083d60d84826a0b4bd17aff35d5
 });
 
 apiRoutes.put('/ressources', function (req,res){
@@ -98,19 +78,6 @@ apiRoutes.delete('/suggestions', function (req,res){
 
 
 
-<<<<<<< HEAD
-// module.exports={
-// 	users:users,
-// 	ressources:ressources,
-// 	suggestions:suggestions
-// };
-
-module.exports=apiRoutes;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f011abe3ec712f42f62220f9bc8549776771dcc2
 // module.exports={
 // 	users:users,
 // 	ressources:ressources,
@@ -118,15 +85,3 @@ module.exports=apiRoutes;
 // };
 
 module.exports = apiRoutes;
-<<<<<<< HEAD
-=======
-module.exports={
-	users:users,
-	ressources:ressources,
-	suggestions:suggestions
-};
->>>>>>> 9e5d06053a64227c2e65ff3c7baafc7aa21594bd
-=======
-
->>>>>>> f011abe3ec712f42f62220f9bc8549776771dcc2
->>>>>>> c30592cea94d1083d60d84826a0b4bd17aff35d5
